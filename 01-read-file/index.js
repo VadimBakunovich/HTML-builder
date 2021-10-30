bd010c9250e4b4f07f52c6d6e00f91b744bdda5e
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-10-30 09:56:21 +0300

    refactor: refactor code in 1 and 2 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs'); 
-const path = require('path');
+const { createReadStream } = require('fs'); 
+const { join } = require('path');
 
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+const stream = createReadStream(join(__dirname, 'text.txt'), 'utf8');
 
 stream.on('data', data => console.log(data.trim()));
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const { stdin } = process;
+const { createWriteStream } = require('fs');
+const { join } = require('path');
+const { stdin, exit } = process;
 
 console.log('Input text to write to the file:');
 
-const stream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const stream = createWriteStream(join(__dirname, 'text.txt'));
 
-stdin.on('data', data => data.toString().trim() == 'exit' ? process.exit() : stream.write(data));
+stdin.on('data', data => data.toString().trim() == 'exit' ? exit() : stream.write(data));
 
-process.on('SIGINT', process.exit);
+process.on('SIGINT', exit);
 
-process.on('exit', () => console.log(`The created file is located here ${path.join(__dirname, 'text.txt')}`));
+process.on('exit', () => console.log(`The created file is located here ${join(__dirname, 'text.txt')}`));
