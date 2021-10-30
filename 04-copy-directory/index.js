e33d214d01ca7fe4884bf070edebf7aa58bb9a24
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-10-30 23:45:52 +0300

    refactor: refactor task 4 code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,30 @@
-const { mkdir, unlink, copyFile } = require('fs/promises');
-const { readdir } = require('fs');
+const { mkdir, readdir, unlink, copyFile } = require('fs/promises');
 const { join } = require('path');
 
 (function copyDir() {
   mkdir(join(__dirname, 'files-copy'), { recursive: true })
     .then(() => {
-      readdir(join(__dirname, 'files-copy'),
-        (err, files) => {
-          if (err) throw err;
-          if (files.length) {
-            for (let i of files) unlink(join(__dirname, 'files-copy', i));
-          }
-        }
-      );
-      readdir(join(__dirname, 'files'),
-        (err, files) => {
-          if (err) throw err;
-          if (files.length) {
-            for (let i of files) {
-              const srcPath = join(__dirname, 'files', i);
-              const destPath = join(__dirname, 'files-copy', i);
-              copyFile(srcPath, destPath);
+      return readdir(join(__dirname, 'files-copy'))
+        .then(
+          files => {
+            if (files.length) {
+              for (let i of files) unlink(join(__dirname, 'files-copy', i));
             }
           }
-        }
-      );
+        );
+    })
+    .then(() => {
+      readdir(join(__dirname, 'files'))
+        .then(
+          files => {
+            if (files.length) {
+              for (let i of files) {
+                const srcPath = join(__dirname, 'files', i);
+                const destPath = join(__dirname, 'files-copy', i);
+                copyFile(srcPath, destPath);
+              }
+            }
+          }
+        );
     });
 })();
