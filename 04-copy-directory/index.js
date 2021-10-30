20fd5608c5efa0d997ab6bb6dee75ca157df2c98
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-10-30 14:53:20 +0300

    feat: implement a function that copies the contents of a folder to another folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const { mkdir, unlink, copyFile } = require('fs/promises');
+const { readdir } = require('fs');
+const { join } = require('path');
 
+(function copyDir() {
+  mkdir(join(__dirname, 'files-copy'), { recursive: true })
+    .then(() => {
+      readdir(join(__dirname, 'files-copy'),
+        (err, files) => {
+          if (err) throw err;
+          if (files.length) {
+            for (let i of files) unlink(join(__dirname, 'files-copy', i));
+          }
+        }
+      );
+      readdir(join(__dirname, 'files'),
+        (err, files) => {
+          if (err) throw err;
+          if (files.length) {
+            for (let i of files) {
+              const srcPath = join(__dirname, 'files', i);
+              const destPath = join(__dirname, 'files-copy', i);
+              copyFile(srcPath, destPath);
+            }
+          }
+        }
+      );
+    });
+})();
