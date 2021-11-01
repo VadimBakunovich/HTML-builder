778b03075605433bfff10b1b652c7a526799ac4d
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-11-01 23:13:12 +0300

    refactor: refactor task 5 code

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,19 @@
-const { writeFile, readdir, readFile } = require('fs/promises');
-const { appendFile } = require('fs');
+const { writeFile, readdir, stat, readFile, appendFile } = require('fs/promises');
 const { join, parse } = require('path');
 
-writeFile(join(__dirname, 'project-dist' ,'bundle.css'), '');
-
-readdir(join(__dirname, 'styles'))
-  .then(
-    files => {
-      for (let i of files) {
-        const ext = parse(join(__dirname, 'styles', i)).ext;
-        if (ext == '.css') {
-          readFile(join(__dirname, 'styles', i))
-            .then (data => appendFile(join(__dirname, 'project-dist' ,'bundle.css'), data, er => er));
-        }
+async function mergeStyles(src, dest) {
+  await writeFile(dest, '');
+  const items = await readdir(src);
+  if (items.length) {
+    for (let item of items) {
+      const stats = await stat(join(src, item));
+      const ext = parse(join(src, item)).ext;
+      if (stats.isFile() && ext == '.css') {
+        const data = await readFile(join(src, item));
+        await appendFile(dest, data);
       }
     }
-  );
+  }
+}
+
+mergeStyles(join(__dirname, 'styles'), join(__dirname, 'project-dist' ,'bundle.css'));
