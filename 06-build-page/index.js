bec170330ca4e4e95fcee907f904fcf73a867458
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-11-01 23:27:42 +0300

    refactor: refactor task 6 code

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,20 +1,20 @@
 const { rm, mkdir, readdir, copyFile, readFile, writeFile, appendFile, stat } = require('fs/promises');
 const { join, parse } = require('path');
 
-async function createHtml(src, dest) {
-  let template = await readFile(dest, { encoding: 'utf8' });
-  const files = await readdir(src);
+async function createHtml(compPath, templPath, dest) {
+  let template = await readFile(templPath, { encoding: 'utf8' });
+  const files = await readdir(compPath);
   if (files.length) {
     for (const file of files) {
-      const name = parse(join(src, file)).name;
-      const ext = parse(join(src, file)).ext;
+      const name = parse(join(compPath, file)).name;
+      const ext = parse(join(compPath, file)).ext;
       if (ext == '.html' && template.includes(`{{${name}}}`)) {
-        const data = await readFile(join(src, file), { encoding: 'utf8' });
+        const data = await readFile(join(compPath, file), { encoding: 'utf8' });
         template = template.replace(`{{${name}}}`, data);
       }
     }
   }
-  writeFile(join(__dirname, 'project-dist' ,'index.html'), template);
+  writeFile(dest, template);
 }
 
 async function createStyles(src, dest) {
@@ -45,7 +45,7 @@ async function copyAssets(src, dest) {
 async function buildPage() {
   await rm(join(__dirname, 'project-dist'), { recursive: true, force: true });
   await mkdir(join(__dirname, 'project-dist'), { recursive: true });
-  createHtml(join(__dirname, 'components'), join(__dirname, 'template.html'));
+  createHtml(join(__dirname, 'components'), join(__dirname, 'template.html'), join(__dirname, 'project-dist' ,'index.html'));
   createStyles(join(__dirname, 'styles'), join(__dirname, 'project-dist' ,'style.css'));
   copyAssets(join(__dirname, 'assets'), join(__dirname, 'project-dist', 'assets'));
 }
