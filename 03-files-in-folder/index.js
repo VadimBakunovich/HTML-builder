8fe988530881eaa4e9758b3a553bf8b1a80c571d
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-11-01 21:59:04 +0300

    refactor: refactor task 3 code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,18 @@
 const { readdir, stat } = require('fs/promises');
 const { join, parse } = require('path');
 
-readdir(join(__dirname, 'secret-folder'))
-  .then(
-    files => {
-      for (let i of files) {
-        stat(join(__dirname, 'secret-folder', i))
-          .then(
-            stats => {
-              if (stats.isFile()) {
-                const name = parse(join(__dirname, 'secret-folder', i)).name;
-                const ext = parse(join(__dirname, 'secret-folder', i)).ext.slice(1);
-                const size = (stats.size / 1024).toFixed(3);
-                console.log(`${name} - ${ext} - ${size}kb`);
-              }
-            }
-          );
+async function getFilesInfo(folderPath) {
+  const items = await readdir(folderPath);
+  if (items.length) {
+    for (let i of items) {
+      const stats = await stat(join(folderPath, i));
+      if (stats.isFile()) {
+        const name = parse(join(folderPath, i)).name;
+        const ext = parse(join(folderPath, i)).ext.slice(1);
+        console.log(`${name} - ${ext} - ${stats.size} byte`);
       }
     }
-  );
+  }
+}
+
+getFilesInfo(join(__dirname, 'secret-folder'));
